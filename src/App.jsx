7801db5193cd8a68353fba3655840c52fc1e7ae2
Manuel J Luna <manueljontinluna@gmail.com>
2025-02-16 21:08:57 -0300

    Terminamos la aplicacion del buscador de peliculas

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import './appStyle.css'
+
+export const App = () => {
+
+    const URL_BASE = 'https://api.themoviedb.org/3/search/movie'
+    const API_KEY = 'YOUR_API_KEY'
+
+    const [search, setSearch] = useState('')
+    const [movie, setMovie] = useState(null)
+
+
+    const handleInputChange = (event) => {
+        setSearch(event.target.value)
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if (search.trim() == '') { return console.error('Debes ingresar una pelicula. App.jsx 16') }
+        fetchMovies()
+    }
+
+    const fetchMovies = async () => {
+        try {
+            const r = await fetch(`${URL_BASE}?query=${search}&api_key=${API_KEY}&language=es-ES`)
+            const data = await r.json()
+            setMovie(data.results)
+        } catch (err) {
+            console.error(err)
+            console.error('Ha ocurrido un error al llamar a la API. App.jsx 22')
+        }
+    }
+
+    return (
+        <>
+            <div className='mainContainer'>
+
+                <h1 className='mainTitle'>Buscador de peliculas</h1>
+
+                <form onSubmit={handleSubmit}>
+                    <input type="text" autoFocus autoComplete='false' placeholder='Escriba el titulo de la pelicula que desee buscar' value={search} onChange={handleInputChange} />
+                    <button type="submit">Buscar</button>
+                </form>
+
+                {
+                    movie &&
+
+                    <div id='movieList'>
+                        {movie.map(movie => (
+                            <div key={movie.id} className='movieContainer'>
+                                <img className='poster' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.original_title} />
+                                <h2>{movie.title}</h2>
+                                <h4>Populariudad: {Math.floor(movie.popularity)}%</h4>
+                                <p>{movie.overview}</p>
+                            </div>
+                        ))}
+                    </div>
+
+                }
+
+            </div>
+        </>
+    )
+}
